a812a9dc0411dbdef1afba3d3820e7f045474618
PranavEakambaram <pranav.eakambaram.09@gmail.com>
2025-11-10 22:04:21 +0000

    menu library

---
+++ Coffee Shop Chatbot/order_menu.py
@@ -0,0 +1,47 @@
+menu = [
+    {
+        "product_id" : "0001",
+        "Name" : "Vanilla Latte",
+        "Price" : "$5.75"
+    },
+    {
+        "product_id" : "0002",
+        "Name" : "Vanilla Espresso",
+        "Price" : "$5.75"
+    },
+    {
+        "product_id" : "0003",
+        "Name" : "Americano",
+        "Price" : "$5.99"
+    },
+    {
+        "product_id" : "0004",
+        "Name" : "Caramel Latte",
+        "Price" : "$5.75"
+    },
+    {
+        "product_id" : "0005",
+        "Name" : "Caramel Cappuccino",
+        "Price" : "$4.75"
+    },
+    {
+        "product_id" : "0006",
+        "Name" : "Vanilla Cappuccino",
+        "Price" : "$5.00"
+    },
+    {
+        "product_id" : "0007",
+        "Name" : "Vanilla Frappuccino",
+        "Price" : "$5.50"
+    },
+    {
+        "product_id" : "0008",
+        "Name" : "Caramel Frappuccino",
+        "Price" : "$6.75"
+    },
+    {
+        "product_id" : "0009",
+        "Name" : "Mocha Frappuccino",
+        "Price" : "$6.75"
+    }
+]
