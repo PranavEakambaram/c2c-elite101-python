efad3bf2174e8e30b871a5e8a6628dfd92ad8193
PranavEakambaram <pranav.eakambaram.09@gmail.com>
2025-11-10 21:47:10 +0000

    base options

---
+++ Coffee Shop Chatbot/chatbot.py
@@ -0,0 +1,33 @@
+
+
+
+
+print("Welcome to Pranav's Coffee Shop!")
+user_name = input ("What's your name?")
+user_age = input("How old are you?")
+print("Hi {user_name}! How can I help you today?")
+
+
+while True:
+    print("Please select one of the following options.")
+    print("1. Order a Coffee")
+    print("2. Build Your Own Coffee")
+    print("3. View Your Cart / Checkout")
+    print("4. Leave Feedback")
+    print("5. Exit")
+
+    choice = input("What would you like to do?")
+
+    if choice = 1:
+        sdfasd
+    elif choice = 2:
+        asdf
+    elif choice = 3:
+        asdf
+    elif choice = 4:
+        asdf
+    elif choice = 5:
+        print("Thank you, {user_name}, Hope to see you again!")
+        break
+    else:
+        print("That's not an option. Please enter a number 1-5.")
