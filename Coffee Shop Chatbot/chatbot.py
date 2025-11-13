b2b17be00781dd62d60d5b431614782918448374
PranavEakambaram <pranav.eakambaram.09@gmail.com>
2025-11-13 12:53:55 +0000

    fixed spacing in choice 2

--- Coffee Shop Chatbot/chatbot.py
+++ Coffee Shop Chatbot/chatbot.py
@@ -6,18 +6,18 @@ cart = []
 print("\n\nWelcome to Pranav's Coffee Shop!")
 user_name = input ("\nWhat's your name?")
 user_age = input("\nHow old are you?")
-print(f"Hi {user_name}! How can I help you today?\n")
+print(f"\n\nHi {user_name}! How can I help you today?\n")
 
 
 while True:
-    print("Please select one of the following options.")
+    print("\n\n\nPlease select one of the following options.")
     print("1. Order a Coffee")
     print("2. Build Your Own Coffee")
     print("3. View Your Cart / Checkout")
     print("4. Leave Feedback")
     print("5. Exit")
 
-    choice = input("What would you like to do?")
+    choice = input("\nWhat would you like to do?")
 
     if choice == "1":
 
@@ -27,7 +27,7 @@ while True:
         for i in menu:
             print(i['product_id'] + ": " + i['Name'] + " - " + i['Price'])
 
-        product_id = input("Enter the product ID of the drink you would like to add to cart. Enter 'Quit' to go back.")
+        product_id = input("\nEnter the product ID of the drink you would like to add to cart. Enter 'Quit' to go back.")
 
         if product_id == "Quit":
             pass
@@ -41,108 +41,108 @@ while True:
 
             if item:
                 cart.append(item)
-                print("Item has been added to cart!")
+                print("\nItem has been added to cart!")
             else:
-                print("Product not found")
+                print("\nProduct not found")
 
 
     elif choice == "2":
 
         base_selected = None
         while (base_selected == None):
-            print("Choose your base:")
+            print("\nChoose your base:\n")
 
             for b in bases:
                 print(b['id'] + ": " + b['Name'])
 
-            base_choice = input ("Enter the ID of the base you would like.")
+            base_choice = input ("\nEnter the ID of the base you would like.")
 
             for b in bases:
                 if b['id'] == base_choice:
-                    base_selected = b['name']
+                    base_selected = b['Name']
                     break
 
             if base_selected == None:
-                print("Please enter a valid base ID")
-        print(f"Your Base: {base_selected}")
+                print("\nPlease enter a valid base ID\n")
+        print(f"\nYour Base: {base_selected}")
 
         flavor_selected = None
         while (flavor_selected == None):
-            print("Choose your flavor")
+            print("\nChoose your flavor\n")
 
             for f in flavors:
                 print(f['id'] + ": " + f['Name'])
 
-            flavor_choice = input ("Enter the ID of the flavor you would like.")
+            flavor_choice = input ("\nEnter the ID of the flavor you would like.")
 
             for f in flavors:
                 if f['id'] == flavor_choice:
-                    flavor_selected = f['name']
+                    flavor_selected = f['Name']
                     break
 
             if flavor_selected == None:
-                print("Please enter a valid flavor ID")
-        print(f"Your Flavor: {flavor_selected}")
+                print("\nPlease enter a valid flavor ID")
+        print(f"\nYour Flavor: {flavor_selected}")
 
         milk_selected = None
         while (milk_selected == None):
-            print("Choose your milk type:")
+            print("\nChoose your milk type:\n")
 
             for m in milks:
                 print(m['id'] + ": " + m['Name'])
 
-            milk_choice = input ("Enter the ID of the milk you would like.")
+            milk_choice = input ("\nEnter the ID of the milk you would like.")
 
             for m in milks:
                 if m['id'] == milk_choice:
-                    milk_selected = m['name']
+                    milk_selected = m['Name']
                     break
 
             if milk_selected == None:
-                print("Please enter a valid milk ID")
-        print(f"Your Milk: {milk_selected}")
+                print("\nPlease enter a valid milk ID")
+        print(f"\nYour Milk: {milk_selected}")
 
         sugar_selected = None
         while (sugar_selected == None):
-            print("Choose your sugar level:")
+            print("\nChoose your sugar level:\n")
 
             for s in sugars:
                 print(s['id'] + ": " + s['Name'])
 
-            sugar_choice = input ("Enter the ID of the sugar level you would like.")
+            sugar_choice = input ("\nEnter the ID of the sugar level you would like.")
 
             for s in sugars:
                 if s['id'] == sugar_choice:
-                    sugar_selected = s['name']
+                    sugar_selected = s['Name']
                     break
 
             if sugar_selected == None:
-                print("Please enter a valid Sugar Level ID")
-        print(f"Your Sugar Level: {sugar_selected}")
+                print("\nPlease enter a valid Sugar Level ID")
+        print(f"\nYour Sugar Level: {sugar_selected}")
 
         drizzle_selected = None
         while (drizzle_selected == None):
-            print("Choose your drizzle: ")
+            print("\nChoose your drizzle: \n")
 
             for d in drizzles:
                 print(d['id'] + ": " + d['Name'])
 
-            drizzle_choice = input ("Enter the ID of the drizzle you would like.")
+            drizzle_choice = input ("\nEnter the ID of the drizzle you would like.")
 
             for d in drizzles:
                 if d['id'] == drizzle_choice:
-                    drizzle_selected = d['name']
+                    drizzle_selected = d['Name']
                     break
 
             if drizzle_selected == None:
-                print("Please enter a valid drizzle ID")
-        print(f"Your Drizzle: {drizzle_selected}")
+                print("\nPlease enter a valid drizzle ID")
+        print(f"\nYour Drizzle: {drizzle_selected}")
 
         print("Your Coffee: ")
         print(f"Base: {base_selected}\nFlavor: {flavor_selected}\nMilk: {milk_selected}\nSuger: {sugar_selected}\nDrizzle: {drizzle_selected}")
 
 
-        add_to_cart = input("Would you like to add your coffee to your cart? Please answer 'Yes' or 'No'")
+        add_to_cart = input("\nWould you like to add your coffee to your cart? Please answer 'Yes' or 'No'")
 
         if (add_to_cart == "Yes"):
             custom_coffee = {
@@ -151,10 +151,10 @@ while True:
                 "Price" : "$6.99"
             }
             cart.append(custom_coffee)
-            print("Your Coffee was added to cart!")
+            print("\nYour Coffee was added to cart!")
 
         elif (add_to_cart == "No"):
-            print("Order cancelled")
+            print("\nOrder cancelled")
 
 
 
